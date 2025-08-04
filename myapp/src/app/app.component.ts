c5eee7e4354b4a7185b1f9b910a21e1cc88ca431
ASWINPRABU <aswinprabu.cse@gmail.com>
2025-08-04 23:36:23 +0530

    day 3

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CounterComponent } from './03-Signals/counter/counter.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CounterComponent],
+  imports: [FormsModule, CounterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -29,4 +30,26 @@ export class AppComponent implements OnInit {
   clickme2(){
     this.clickcount--;
   }
+  
+  moves= [
+    `move 1`,
+    `move 2`,
+    `move 3`,
+    `move 4`,
+    `move 5`,
+    `move 6`,
+    `move 7`,
+    `move 8`
+  ]
+  fruits = [
+    `apple`,
+    `banana`,
+    `cherry`,
+    `date`,
+    `elderberry`,
+    `fig`,
+    `grape`
+  ]
+
+
 }
